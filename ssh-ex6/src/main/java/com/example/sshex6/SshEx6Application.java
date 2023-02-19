9e815794189869e9fc1d8bf0e4c4b469b525ffa4
guesanjo <guesanjoupv@gmail.com>
2023-02-19 16:59:25 +0100

    skeleton ex6

---
+++ ssh-ex6/src/main/java/com/example/sshex6/SshEx6Application.java
@@ -0,0 +1,13 @@
+package com.example.sshex6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshEx6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SshEx6Application.class, args);
+	}
+
+}
---
+++ ssh-ex6/src/test/java/com/example/sshex6/SshEx6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sshex6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshEx6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
