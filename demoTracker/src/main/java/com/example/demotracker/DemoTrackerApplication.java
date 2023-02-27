b41c127cfbeb034b65706e7d602ae3d699abf24d
Guesanjo <guesanjoupv@gmail.com>
2023-02-27 17:53:49 +0100

    creacion API tracker postgre
    con setup base de datos

---
+++ demoTracker/src/main/java/com/example/demotracker/DemoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTrackerApplication.class, args);
+    }
+
+}
---
+++ demoTracker/src/test/java/com/example/demotracker/DemoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
